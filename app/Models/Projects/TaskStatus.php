d56f12bb4a6d202a9083dbb0b23aacc2ca08d904
Kovah <mail@kovah.de>
2016-10-06 10:58:30 +0200

    Moves Currency model into settings folder, adds missing Task Status model

---
+++ app/Models/Projects/TaskStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Projects;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Task
+ * @package App\Models\Projects
+ */
+class TaskStatus extends Model
+{
+    // Table definition
+    protected $table = 'task_statuses';
+}
