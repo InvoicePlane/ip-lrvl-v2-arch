802b8f921b36dd2969ca94df639b244fe634559b
Kovah <mail@kovah.de>
2016-10-07 12:33:51 +0200

    Sets the database fields for all user models

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -3,6 +3,7 @@
 namespace App\Models\Users;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class User
@@ -10,6 +11,24 @@ use Illuminate\Database\Eloquent\Model;
  */
 class User extends Model
 {
+    use SoftDeletes;
+
     // Table definition
     protected $table = 'users';
+
+    // Fillable db fields
+    protected $fillable = [
+        'name',
+        'email',
+        'client_contact_id',
+        'company_id',
+        'is_disabled',
+        'password',
+    ];
+
+    // Hidden db fields
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
