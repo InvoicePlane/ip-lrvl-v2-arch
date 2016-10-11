9be416ff61ee03e5758f6917811e30a3f4e9734b
Kovah <mail@kovah.de>
2016-10-11 10:57:45 +0200

    Adds all relations to the user model

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -31,4 +31,73 @@ class User extends Model
         'password',
         'remember_token',
     ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relations
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Returns the associated client contact
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function clientContact()
+    {
+        return $this->belongsTo('App\Models\Clients\Contact');
+    }
+
+    /**
+     * Returns the associated company
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function company()
+    {
+        return $this->belongsTo('App\Models\Settings\Company');
+    }
+
+    /**
+     * Returns all authored attachments
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function attachments()
+    {
+        return $this->hasMany('App\Models\Attachment', 'uploader_id');
+    }
+
+    /**
+     * Returns all authored notes
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function notes()
+    {
+        return $this->hasMany('App\Models\Note', 'author_id');
+    }
+
+    /**
+     * Returns all projects where the user is leader
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects()
+    {
+        return $this->hasMany('App\Models\Projects\Project', 'leader_id');
+    }
+
+    /**
+     * Returns all authored attachments
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function timeTrackings()
+    {
+        return $this->hasMany('App\Models\Projects\TimeTrackings', 'user_id');
+    }
+
+    /**
+     * Returns all authored vouchers
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function vouchers()
+    {
+        return $this->hasMany('App\Models\Vouchers\Voucher', 'user_id');
+    }
 }
