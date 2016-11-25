24e3616e84d4bb79a49a5d6f1a3085d2c2ed2e74
Kovah <mail@kovah.de>
2016-11-25 10:41:18 +0100

    Updates Client and Settings factories to generate fake data

--- database/factories/ClientModelsFactory.php
+++ database/factories/ClientModelsFactory.php
@@ -5,3 +5,56 @@
 | Client Models Factories
 |--------------------------------------------------------------------------
 */
+
+// Address Factory
+$factory->define(\App\Models\Clients\Address::class, function (Faker\Generator $faker) {
+    $faker->addProvider(new \Faker\Provider\en_US\Address($faker));
+
+    $address = $faker->streetAddress . "\n";
+    $address .= $faker->city . ' ' . $faker->state . ' ' . $faker->postcode . "\n";
+    $address .= $faker->country;
+
+    return [
+        'client_id' => null,
+        'address_block' => $address,
+        'country_code' => $faker->countryCode,
+        'is_disabled' => $faker->boolean(10),
+    ];
+});
+
+// Client Factory
+$factory->define(\App\Models\Clients\Client::class, function (Faker\Generator $faker) {
+    $company = $faker->company;
+
+    return [
+        'name' => $company . ' ' . $faker->companySuffix,
+        'short_name' => $company,
+        'main_contact_id' => null,
+        'main_address_id' => null,
+        'language_id' => \App\Models\Settings\Language::inRandomOrder()->first()->id,
+        'company_id' => \App\Models\Settings\Company::inRandomOrder()->first()->id,
+        'telephone' => $faker->phoneNumber,
+        'fax' => ($faker->boolean(25) ? $faker->phoneNumber : null),
+        'email' => $faker->companyEmail,
+        'web' => $faker->url,
+        'is_company' => $faker->boolean(50),
+        'is_vendor' => $faker->boolean(25),
+        'is_disabled' => $faker->boolean(10),
+    ];
+});
+
+// Contact Factory
+$factory->define(\App\Models\Clients\Contact::class, function (Faker\Generator $faker) {
+    return [
+        'title' => ($faker->boolean(33) ? $faker->title : null),
+        'forename' => $faker->firstName,
+        'surname' => $faker->lastName,
+        'client_id' => null,
+        'address_id' => null,
+        'telephone' => $faker->phoneNumber,
+        'mobile' => $faker->phoneNumber,
+        'fax' => ($faker->boolean(25) ? $faker->phoneNumber : null),
+        'email' => $faker->companyEmail,
+        'is_disabled' => $faker->boolean(10),
+    ];
+});
--- database/factories/SettingsModelsFactory.php
+++ database/factories/SettingsModelsFactory.php
@@ -5,3 +5,51 @@
 | Settings Models Factories
 |--------------------------------------------------------------------------
 */
+
+// Company Factory
+$factory->define(\App\Models\Settings\Company::class, function (Faker\Generator $faker) {
+    $company = $faker->company;
+
+    return [
+        'name' => $company . ' ' . $faker->companySuffix,
+        'short_name' => $company,
+        'address_id' => null,
+        'telephone' => $faker->phoneNumber,
+        'fax' => ($faker->boolean(25) ? $faker->phoneNumber : null),
+        'email' => $faker->companyEmail,
+        'web' => $faker->url,
+        'is_disabled' => $faker->boolean(10),
+    ];
+});
+
+// Currency Factory
+$factory->define(\App\Models\Settings\Currency::class, function (Faker\Generator $faker) {
+    $currency_symbols = collect(['$', '€', '¥', '£']);
+    $thousand_separator = $faker->randomElement([',', '.', ' ']);
+    $decimal_separator = ($thousand_separator == ',' ? '.' : ',');
+
+    return [
+        'name' => $faker->currencyCode,
+        'currency_symbol' => $currency_symbols,
+        'symbol_placement' => $faker->boolean(50),
+        'thousand_separator' => $thousand_separator,
+        'decimal_separator' => $decimal_separator,
+        'decimal_places' => rand(2, 5),
+        'is_disabled' => $faker->boolean(10),
+    ];
+});
+
+// Language Factory
+$factory->define(\App\Models\Settings\Language::class, function (Faker\Generator $faker) {
+
+    $locale = $faker->locale;
+    $codes = explode('_', $locale);
+
+    return [
+        'title' => $locale,
+        'lang_code' => $codes[0],
+        'country_code' => $codes[1],
+        'is_disabled' => $faker->boolean(10),
+        'last_update' => \Carbon\Carbon::now()->subDays(rand(5, 20)),
+    ];
+});
